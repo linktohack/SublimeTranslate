ff510f04e636cc0b6a43f645b7a3ad81eb164325
Quang Linh LE <linktohack@gmail.com>
2013-10-27 18:20:28 +0100

    Remove unnecessary module
    They tend to load Exeptions, may be will be re-added later

--- google_translate.py
+++ google_translate.py
@@ -1,13 +1,6 @@
 import sublime, sublime_plugin
 from GoogleTranslate.google_translate_python.translate import Translator
-try:
-    import urllib2 as request
-    from urllib import quote
-except:
-    from urllib import request
-    from urllib.parse import quote
 
-translator = Translator(to_lang='fr')
 
 class TranslateCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
