ad7603c876ece31c37007152c89d369a1f59bf19
Quang Linh LE <linktohack@gmail.com>
2013-10-27 18:23:14 +0100

    Add settings file

--- google_translate.py
+++ google_translate.py
@@ -3,7 +3,12 @@ from GoogleTranslate.google_translate_python.translate import Translator
 
 
 class TranslateCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		for region in self.view.sel():
-			translation = translator.translate(self.view.substr(region))
-			self.view.replace(edit, region, translation)
+    def run(self, edit):
+        settings = sublime.load_settings('GoogleTranslate.sublime-settings')
+        from_lang =  settings.get('from_lang', '')
+        to_lang =  settings.get('to_lang', '')
+        translator = Translator(from_lang=from_lang, to_lang=to_lang)
+        
+        for region in self.view.sel():
+            translation = translator.translate(self.view.substr(region))
+            self.view.replace(edit, region, translation)
