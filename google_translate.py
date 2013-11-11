cfb30d1d67d33fcc7082976a7bb31bb1fd2f42e6
Quang-Linh LE <linktohack@gmail.com>
2013-11-11 01:00:03 +0100

    from_lang and to_lang now can be use as args

--- google_translate.py
+++ google_translate.py
@@ -3,12 +3,17 @@ from GoogleTranslate.google_translate_python.translate import Translator
 
 
 class TranslateCommand(sublime_plugin.TextCommand):
-    def run(self, edit, extend=False):
+    def run(self, edit, extend=False, from_lang='', to_lang=''):
         settings = sublime.load_settings('GoogleTranslate.sublime-settings')
-        from_lang =  settings.get('from_lang', '')
-        to_lang =  settings.get('to_lang', '')
+        if not extend:
+            extend = settings.get('extend', False)
+        if from_lang == '':
+            from_lang = settings.get('from_lang', '')
+        if to_lang == '':
+            to_lang = settings.get('to_lang', 'en')
+
         translator = Translator(from_lang=from_lang, to_lang=to_lang)
-        
+
         for region in self.view.sel():
             translation = translator.translate(self.view.substr(region))
             if extend:
